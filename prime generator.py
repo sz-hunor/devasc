6483500953029a6c36f07143a3992e6939741a58
Hunor <s.hunor.g@gmail.com>
2023-01-05 22:04:26 +0100

    added description

--- prime generator.py
+++ prime generator.py
@@ -1,5 +1,12 @@
 from itertools import count, islice
+"""Itertools.islice() - Performs lazy slicing of an iterator
+Itertools.count() - Lazily generates a sequence of infinite numbers
 
+To find the first N primes, it is possible to create a generator that will produce an infinite sequence of integers 
+with itertools.count(), pass these through a generator expression that checks weather each integer is a prime 
+using an external function and then use itertools.islice() to capture the slice required slice
+This creates a generator object that can then be force evaluated using the list() constructor
+"""
 
 def is_prime(x):
     if x > 2:
